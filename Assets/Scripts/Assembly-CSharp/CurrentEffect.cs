77d03c3c4b7025891ec61b189ec4b4b36c2883bc
Dawid Krystian Czaplicki <100421659+dawc17@users.noreply.github.com>
2026-08-29 05:53:05 +0200

    Move effect follow diagnostics into SF2DE

--- Assets/Scripts/Assembly-CSharp/CurrentEffect.cs
+++ Assets/Scripts/Assembly-CSharp/CurrentEffect.cs
@@ -1,3 +1,4 @@
+using SF2DE.Rendering.Diagnostics;
 using SF2DE.Rendering.Interpolation;
 using UnityEngine;
 
@@ -15,13 +16,7 @@ public class CurrentEffect
 
 	private FightTransformInterpolation _Interpolation;
 
-	private Vector3 _PreviousAnchor;
-
-	private bool _HasPreviousAnchor;
-
-	private int _LoggedAnchorJumps;
-
-	private int _PreviousDirection;
+	private readonly EffectFollowDiagnostics _Diagnostics = new EffectFollowDiagnostics();
 
 	public CurrentEffect(Model GIAMLEDNFJD, ActionEffect FNNOHPEMKMB, GameObject GHDAPMGLICD, CocosAnimation EDMCLHEOJGD)
 	{
@@ -39,9 +34,6 @@ public class CurrentEffect
 			}
 			_Interpolation.Snap(EGJHGBCEPHO.transform.localPosition, EGJHGBCEPHO.transform.localRotation);
 		}
-		_HasPreviousAnchor = false;
-		_LoggedAnchorJumps = 0;
-		_PreviousDirection = 0;
 	}
 
 	public void HJGPLENNFCK()
@@ -50,25 +42,7 @@ public class CurrentEffect
 		ModelConditions kDOGKKGDOBK = ACENLMONNPA.EBABHGHPLFK();
 		Vector3f eMAFACPEPDK = Vector3f.op_Implicit(LLOLBKJMKNC.ECJPLFFAMJO().EMGKDOAMBOH(kDOGKKGDOBK));
 		Vector3 anchor = new Vector3(eMAFACPEPDK.GILCBJJPKBK(), eMAFACPEPDK.OBIMBNIBEFG(), eMAFACPEPDK.KMFEKANLCFO());
-		if (_HasPreviousAnchor)
-		{
-			float anchorStep = Vector3.Distance(_PreviousAnchor, anchor);
-			if ((anchorStep > 40f || (_PreviousDirection != 0 && _PreviousDirection != num)) && _LoggedAnchorJumps < 5)
-			{
-				Debug.LogWarning("[EffectTransform] follow-jump actor=" + ACENLMONNPA.get_Name() +
-					" action=" + LLOLBKJMKNC.get_Name() +
-					" sequence=" + LLOLBKJMKNC.EPDMGFELIMC() +
-					" step=" + anchorStep +
-					" from=" + _PreviousAnchor.x + "," + _PreviousAnchor.y +
-					" to=" + anchor.x + "," + anchor.y +
-					" direction=" + _PreviousDirection + "->" + num +
-					" frame=" + Time.frameCount);
-				_LoggedAnchorJumps++;
-			}
-		}
-		_PreviousAnchor = anchor;
-		_PreviousDirection = num;
-		_HasPreviousAnchor = true;
+		_Diagnostics.Observe(ACENLMONNPA, LLOLBKJMKNC, anchor, num);
 		Quaternion rotation = _Interpolation.CurrentRotation;
 		Vector2f hEJKLMNOLLG = LLOLBKJMKNC.MABFDDNEOGO().HLBBNCBJHGB(kDOGKKGDOBK);
 		if (hEJKLMNOLLG.GILCBJJPKBK() != 0f || hEJKLMNOLLG.OBIMBNIBEFG() != 0f)
